2dfb83e454e6e38e1ad108381ae3ebae4611d3c2
VuNamKhanh485 <227631105+VuNamKhanh485@users.noreply.github.com>
2026-08-20 19:01:49 +0700

    Update ImportOrderController.java

--- src/java/controller/ImportOrderController.java
+++ src/java/controller/ImportOrderController.java
@@ -18,29 +18,7 @@ import jakarta.servlet.http.HttpServletResponse;
  */
 public class ImportOrderController extends HttpServlet {
    
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ImportOrderController</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ImportOrderController at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    } 
+   
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
@@ -53,7 +31,7 @@ public class ImportOrderController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+       
     } 
 
     /** 
@@ -66,16 +44,6 @@ public class ImportOrderController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+       
     }
-
-    /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
