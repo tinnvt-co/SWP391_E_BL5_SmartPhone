64de34023121492aac90374af497df3f267706f5
VuNamKhanh485 <227631105+VuNamKhanh485@users.noreply.github.com>
2026-08-28 17:22:22 +0700

    11

--- src/java/config/ReviewMigrationRunner.java
+++ src/java/config/ReviewMigrationRunner.java
@@ -1,8 +1,6 @@
 package config;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
